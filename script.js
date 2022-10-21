369c7f170e7284ba76957bba147792029ad359e8
dheerajy1 <44141999+dheerajy1@users.noreply.github.com>
2022-10-21 17:36:01 +0530

    added style

--- script.js
+++ script.js
@@ -1,36 +1,52 @@
 function bakcal() {
-    var a = document.getElementById("a").value;
+    var a = parseInt(document.getElementById("a").value);
 
-    document.getElementById("a1").innerHTML = "One full bakup - " + (a > 12 ? a - 12 : a.slice(1, 2)) + (a.slice(0, 2) < 12 ? ' AM' : ' PM');
-    console.log("hi");
+    document.getElementById("a1").innerHTML = "One Full Bakup - " + (a > 12 ? a - 12 : a) + (a < 12 ? ' AM' : ' PM');
+    //console.log("a="+a);
 
     var b = parseInt(document.getElementById("b").value);
     var b1 = parseInt(document.getElementById("b1").value);
-   // console.log("b=" + b); //4
-    //console.log("b1="+b1); //0
+    //console.log("b=" + b);
+    //console.log("b1="+b1); 
 
     var d = parseInt(document.getElementById("d").value);
-    //console.log("d=" + d); //21
+    //console.log("d=" + d); 
+
     while (b1 <= d) {
         b1 = b1 + b;
     }
-    //console.log("b1=" + (b1 - b)); //8
+    //console.log("b1=" + (b1 - b));
 
-    document.getElementById("b2").innerHTML = "One Diff Backup - " + ((b1 - b) > 12 ? b1 - b - 12 : b1 - b) + ((b1 - b) < 12 ? ' AM' : ' PM');
-    //console.log(d - b1 + b); //1
-    var d1 = document.getElementById("d").value; var c1;
+    /*document.getElementById("b2").innerHTML = "One Diff Backup - " + ((b1 - b) > 12 ? b1 - b - 12 : b1 - b) + ((b1 - b) < 12 ? ' AM' : ' PM');
+    document.getElementById("b2").innerHTML = "One Diff Backup - " + 
+    ((b1 - b) > 12 ? ((b1 - b - 12)+" PM") : (b1 - b+" AM"));*/
+
+    //console.log(d - b1 + b);
+
+
+    var d1 = document.getElementById("d").value;
+    var c1, c = parseInt(document.getElementById("c").value);
     //console.log(d1.slice(-2)); //49
     //console.log(49/15); 3.2
-    
+
     /*if((d - b1 + b) != 0){
         c1 = ((d - b1 + b) * 60) / 15 + parseInt((d1.slice(-2)) / 15);
     } 
     //console.log(c1);
     document.getElementById("c1").innerHTML = "Log Backups - " + c1 + " log files"; 
     */
+    if ((b1 - b) > a) {
+        document.getElementById("b2").innerHTML = "One Diff Backup - " +
+            ((b1 - b) > 12 ? ((b1 - b - 12) + " PM") : (b1 - b + " AM"));
+        document.getElementById("c1").innerHTML = "Log Backups - " +
+            ((d - b1 + b) != 0 ? (((d - b1 + b) * 60) / c + parseInt((d1.slice(-2)) / c)) : NaN)
+            + " log files";
 
-    document.getElementById("c1").innerHTML = "Log Backups - " + ((d - b1 + b) != 0 ?(c1 = ((d - b1 + b) * 60) / 15 + parseInt((d1.slice(-2)) / 15)) : NaN )
-     + " log files";
-
+        document.getElementById("c2").innerHTML = "OR" + '<br>' + " Full Back with Purely Log Backups - " + ((d - a) * 60 / c + parseInt(d1.slice(-2) / c)) + " log files";
+    }
+    else {
+        document.getElementById("c1").innerHTML = "Log Backups - " +
+            ((d - a) != 0 ? (((d - a) * 60) / c) : NaN) + " log files";
+    }
 
 }
